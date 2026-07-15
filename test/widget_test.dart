029f5a87e12309aa9fb7b30f9e4708b905411d54
mops-sobakin <mops-sobakin@users.noreply.github.com>
2026-07-15 13:11:26 +0300

    clean: remove all Flutter/Tauri files, keep only index.html

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:aqua_code/app.dart';
-
-void main() {
-  testWidgets('Aqua Code app should render', (WidgetTester tester) async {
-    await tester.pumpWidget(const AquaCodeApp());
-    expect(find.text('Aqua Code'), findsOneWidget);
-  });
-}
